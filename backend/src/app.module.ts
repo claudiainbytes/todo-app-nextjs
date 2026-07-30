393610894cb94fe8f200aa7171f0386fa13cb2b1
claudiainbytes <claudiaeforero@hotmail.com>
2026-07-30 12:08:29 -0500

    C010. Create ToDos module with NestJS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 // import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, TodosModule],
   //controllers: [AppController],
   //providers: [AppService],
 })
