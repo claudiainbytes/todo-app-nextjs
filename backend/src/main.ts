306650ad3e8831a510f734ed773966cd201523e2
claudiainbytes <claudiaeforero@hotmail.com>
2026-09-02 19:32:00 -0500

    C025. Adding vercel.json again resolving CORS issues

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,8 +13,6 @@ async function bootstrap() {
   );
   app.enableCors({
     origin: [process.env.CORS_BASE_URL_1, process.env.CORS_BASE_URL_2],
-    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 4000);
